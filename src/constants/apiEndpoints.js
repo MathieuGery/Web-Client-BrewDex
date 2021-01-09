3bc573f2a27bbca635f51be076e0515d6967bb84
mathieugery <mathieu.gery@epitech.eu>
2021-01-09 23:39:00 +0100

    [+] Change api route

--- src/constants/apiEndpoints.js
+++ src/constants/apiEndpoints.js
@@ -1,4 +1,4 @@
 // const BASE_URL = "https://api.pickeat.fr";
-const BASE_URL = "http://localhost:3000";
+const BASE_URL = "https://brewdex-api.gery.me";
 
 export const SIGN_IN_URL = BASE_URL + '/api/auth/register';
